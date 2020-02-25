2ffbd3a1c3948fc2f1603a101c8b7301ee6e4a5a
wooknick <wooknick@naver.com>
2020-02-25 23:19:48 +0900

    add global-styles

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { graphql } from "react-apollo";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import { reset } from "styled-reset";
+import { ThemeProvider } from "styled-components";
 import theme from "../../theme";
 import AppPresenter from "./AppPresenter";
 import { IS_LOGGED_IN } from "./AppQuries";
 
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-`;
 const AppContainer = ({ data }: { data?: any }) => (
   <ThemeProvider theme={theme}>
-    <GlobalStyle />
     <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
   </ThemeProvider>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import { ApolloProvider } from "react-apollo";
 import ReactDOM from "react-dom";
 import client from "./apollo";
 import App from "./Components/App";
+import GlobalStyle from "./global-styles";
 
 ReactDOM.render(
   <ApolloProvider client={client}>
+    <GlobalStyle />
     <App />
   </ApolloProvider>,
   document.getElementById("root")
