077f70d93d9197127369e47d920545b269051fb9
wooknick <wooknick@naver.com>
2020-04-13 03:58:33 +0900

    add SocialLogin

---
+++ src/Routes/SocialLogin/SocialLoginContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SocialLoginPresenter from "./SocialLoginPresenter";
+
+const SocialLoginContainer: React.FunctionComponent = () => (
+  <SocialLoginPresenter />
+);
+
+export default SocialLoginContainer;
