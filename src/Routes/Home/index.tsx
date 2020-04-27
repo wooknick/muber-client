f49f018531226f5b8967a7ea0615508cbb2adeaa
wooknick <wooknick@naver.com>
2020-04-28 04:13:20 +0900

    hide key

--- src/Routes/Home/index.tsx
+++ src/Routes/Home/index.tsx
@@ -1,7 +1,6 @@
 import { GoogleApiWrapper } from "google-maps-react";
-import { MAPS_KEY } from "../../keys";
 import HomeContainer from "./HomeContainer";
 
 export default GoogleApiWrapper({
-  apiKey: MAPS_KEY,
+  apiKey: process.env.REACT_APP_MAPS_KEY || "",
 })(HomeContainer);
