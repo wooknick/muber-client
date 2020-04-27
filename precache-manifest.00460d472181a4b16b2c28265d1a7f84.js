a4fff2133eca142f481fb0e72afa82cbba19ba74
wooknick <wooknick@naver.com>
2020-04-28 03:28:42 +0900

    Updates

---
+++ precache-manifest.00460d472181a4b16b2c28265d1a7f84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7888e7891dc202479872ac838380ed24",
+    "url": "/muber-client/index.html"
+  },
+  {
+    "revision": "741c5ab0faa0d326df74",
+    "url": "/muber-client/static/css/2.00649687.chunk.css"
+  },
+  {
+    "revision": "741c5ab0faa0d326df74",
+    "url": "/muber-client/static/js/2.35be7902.chunk.js"
+  },
+  {
+    "revision": "428c7b193724e715944e57e016f42a30",
+    "url": "/muber-client/static/js/2.35be7902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352c4e7ac8f75cffb597",
+    "url": "/muber-client/static/js/main.663434b7.chunk.js"
+  },
+  {
+    "revision": "0a78d96f19a875adee38",
+    "url": "/muber-client/static/js/runtime-main.840ac2ba.js"
+  },
+  {
+    "revision": "30dbc02e618c7eb137a1bdf4fd60c589",
+    "url": "/muber-client/static/media/bg.30dbc02e.jpg"
+  }
+]);
